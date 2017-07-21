3486499b15eb3814278b387b67f99db12746ad92
AndriyCO <Aolijnyk73@gmail.com>
2017-07-22 02:00:58 +0300

    first comett in next line project10

--- ProgectCreatingInEclipse10/src/pcie_package10/Main.java
+++ ProgectCreatingInEclipse10/src/pcie_package10/Main.java
@@ -3,8 +3,7 @@ package pcie_package10;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("this message for paralel-brunch p11");
 	}
 
 }
